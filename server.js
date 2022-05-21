46d1c274c399191fd3b08162f2497039f2875310
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-05-21 17:03:46 +0200

    initialize server with socket io

---
+++ server.js
@@ -0,0 +1,16 @@
+const server = require("http").createServer();
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+const port = process.env.PORT || 3000;
+
+server.listen(port);
+console.log("server is running on port " + port);
+
+io.on("connection", (socket) => {
+  console.log("user connected");
+});

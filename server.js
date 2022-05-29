adccc3eb6d126732ef052e53d2f561dfad16e5a1
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-05-29 05:12:00 +0200

    306. Listening for Events in the Pong Server

--- server.js
+++ server.js
@@ -10,7 +10,16 @@ const port = process.env.PORT || 3000;
 
 server.listen(port);
 console.log("server is running on port " + port);
-
+let readyPlayerCount = 0;
 io.on("connection", (socket) => {
   console.log("user connected from backend with socket id: " + socket.id);
+  socket.on("ready", () => {
+    console.log("Player ready", socket.id);
+
+    readyPlayerCount++;
+
+    if (readyPlayerCount === 2) {
+      //broadcast('startGame')
+    }
+  });
 });
